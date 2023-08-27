e564720ff7d7bb6496cb455ca4236ba845c895ae
alexanderworldercraft <139764744+alexanderworldercraft@users.noreply.github.com>
2023-08-27 15:36:53 +0200

    Optimisation vitesse de chargement

--- js/chargement.js
+++ js/chargement.js
@@ -1,5 +1,20 @@
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
     var loading = document.getElementById('loading');
     loading.style.display = 'none';
-  });
-  
+
+    // Commencez à charger les images
+    loadLazyImages();
+});
+
+function loadLazyImages() {
+    // Sélectionnez toutes les images ayant un attribut "data-src"
+    var images = document.querySelectorAll('img[data-src]');
+
+    images.forEach(function (img) {
+        img.setAttribute('src', img.getAttribute('data-src'));
+        img.onload = function () {
+            // Supprimez l'attribut "data-src" une fois l'image chargée
+            img.removeAttribute('data-src');
+        };
+    });
+}
