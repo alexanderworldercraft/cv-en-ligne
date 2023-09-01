afe5f4c754084e1b66042cb07859e3717ab8facf
alexanderworldercraft <139764744+alexanderworldercraft@users.noreply.github.com>
2023-09-01 19:08:55 +0200

    avancement

--- js/firebase-contact.js
+++ js/firebase-contact.js
@@ -5,8 +5,7 @@ const firebaseConfig = {
     projectId: "curriculum-vita-tarroz-patrick",
     storageBucket: "curriculum-vita-tarroz-patrick.appspot.com",
     messagingSenderId: "526705184095",
-    appId: "1:526705184095:web:0e01008a150866b7497aea",
-    measurementId: "G-17HVHBXJ2H"
+    appId: "1:526705184095:web:0e01008a150866b7497aea"
   };
 firebase.initializeApp(firebaseConfig);
 
@@ -14,41 +13,58 @@ firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
 // Écouteur d'événement pour le bouton d'envoi
-document.getElementById('envoyer-button').addEventListener('click', function(event) {
-  event.preventDefault();
+document.getElementById('envoyer-button').addEventListener('click', function (event) {
+    event.preventDefault();
 
-  // Récupération des valeurs du formulaire
-  const nom = document.getElementById('nom').value;
-  const prenom = document.getElementById('prenom').value;
-  const tel = document.getElementById('tel').value;
-  const email = document.getElementById('email').value;
-  const commentaire = document.getElementById('commentaire').value;
+    // Récupération des valeurs du formulaire
+    const nom = document.getElementById('nom').value;
+    const prenom = document.getElementById('prenom').value;
+    const tel = document.getElementById('tel').value;
+    const email = document.getElementById('email').value;
+    const commentaire = document.getElementById('commentaire').value;
 
-  // Ajout des données à Firestore
-  db.collection('contacts').add({
-    nom: nom,
-    prenom: prenom,
-    tel: tel,
-    email: email,
-    commentaire: commentaire
-  })
-  .then((docRef) => {
-    console.log("Document écrit avec l'ID : ", docRef.id);
+    // Ajout des données à Firestore
+    db.collection('contacts').add({
+        nom: nom,
+        prenom: prenom,
+        tel: tel,
+        email: email,
+        commentaire: commentaire
+    })
+    .then((docRef) => {
+        console.log("Document écrit avec l'ID : ", docRef.id);
 
-    // Affichage du message de succès
-    const dialog = document.getElementById('dialog');
-    dialog.style.display = 'block';
-    dialog.innerHTML = 'Votre message a été envoyé avec succès !';
-    dialog.title = 'Succès';
-  })
-  .catch((error) => {
-    console.error("Erreur en ajoutant le document : ", error);
-    
-    // Affichage du message d'erreur
-    const dialog = document.getElementById('dialog');
-    dialog.style.display = 'block';
-    dialog.innerHTML = 'Une erreur est survenue. Veuillez réessayer.';
-    dialog.title = 'Erreur';
-  });
+        // Affichage du message de succès
+        const dialog = document.getElementById('dialog');
+        dialog.style.display = 'block';
+        dialog.innerHTML = 'Votre message a été envoyé avec succès !';
+        dialog.title = 'Succès';
+
+        // Réinitialisation des champs du formulaire
+        document.getElementById('nom').value = '';
+        document.getElementById('prenom').value = '';
+        document.getElementById('tel').value = '';
+        document.getElementById('email').value = '';
+        document.getElementById('commentaire').value = '';
+    })
+    .catch((error) => {
+        console.error("Erreur en ajoutant le document : ", error);
+
+        // Affichage du message d'erreur
+        const dialog = document.getElementById('dialog');
+        dialog.style.display = 'block';
+        dialog.innerHTML = 'Une erreur est survenue. Veuillez réessayer.';
+        dialog.title = 'Erreur';
+
+        //Gestion des différents erreurs
+        //if (error.code === 'network-error') {  // Hypothetical error code
+        //        dialog.innerHTML = 'Une erreur de réseau est survenue. Veuillez vérifier votre connexion Internet.';
+        //      } else if (error.code === 'permission-denied') {  // Hypothetical error code
+        //        dialog.innerHTML = 'Vous n\'avez pas les permissions nécessaires pour effectuer cette opération.';
+        //      } else {
+        //        dialog.innerHTML = 'Une erreur inconnue est survenue. Veuillez réessayer.';
+        //      }
+
+    });
 });
 
